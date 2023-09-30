ccfbb7e96eb69b18aac3edee170390d01ccc7993
Areesh <areesh.192003@gmail.com>
2023-09-30 18:44:15 +0500

    working with RESTFULL API and managing views

--- lib/res/Routes/routing.dart
+++
@@ -1,9 +0,0 @@
-import 'package:filmfolio/views/SplashScreen.dart';
-import 'package:filmfolio/views/mainpage_view.dart';
-import 'package:flutter/cupertino.dart';
-
-import 'routeName.dart';
-
-final Map<String, WidgetBuilder> routes = {
-  RouteName.mainpage: (BuildContext context) => Mainpage(),
-};
